bfdc72366af71b9dca26fd9dbd4a62ef5eb75eac
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2024-03-19 21:47:32 -0300

    todos clientes e editar contatos

--- listaContatos/src/pages/Home/index.js
+++ listaContatos/src/pages/Home/index.js
@@ -18,6 +18,10 @@ export default function Home() {
     navigation.navigate('Creditos')
   }
 
+  const navegaTodosContatos = () => {
+    navigation.navigate('TodosContatos')
+  }
+
   return (
     <View style={styles.container}>
       <View  style={{display:'flex', flexDirection:'row',justifyContent:"space-between",width:'100%', height:"24%",padding:10}}>
@@ -28,7 +32,7 @@ export default function Home() {
       </View>
       <View  style={{display:'flex', flexDirection:'row',justifyContent:"space-between",width:'100%', height:"24%",padding:10}}>
           
-            <TouchableOpacity style={{width:"45%",height:"100%",  backgroundColor:"blue", display:'flex', justifyContent:"center",alignItems:"center"}} title='Cadastrar Contato' onPress={navegaHome} ><Text style={{color:"white"}}>Todos Contatos</Text></TouchableOpacity>
+            <TouchableOpacity style={{width:"45%",height:"100%",  backgroundColor:"blue", display:'flex', justifyContent:"center",alignItems:"center"}} title='Cadastrar Contato' onPress={navegaTodosContatos} ><Text style={{color:"white"}}>Todos Contatos</Text></TouchableOpacity>
             <TouchableOpacity style={{width:"45%", height:"100%", backgroundColor:"blue",display:'flex', justifyContent:"center",alignItems:"center"}} title='BuscarContato' onPress={navegaCreditos} ><Text style={{color:"white"}}>Creditos</Text></TouchableOpacity>
            
       </View>
--- listaContatos/src/pages/TodosContatos/index.js
+++ listaContatos/src/pages/TodosContatos/index.js
@@ -1,6 +1,9 @@
+import React,{useCallback} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, SafeAreaView } from 'react-native';
+import { StyleSheet, FlatList, Text, TextInput ,TouchableOpacity, View, SafeAreaView, Alert } from 'react-native';
+import {useRoute, useFocusEffect } from '@react-navigation/native'
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 import api from '../../services/api/api'
 
@@ -10,13 +13,16 @@ export default function TodosContatos() {
     const [Email, setEmail] = useState('');
     const [Celular, setCelular] = useState();
     const [Telefone, setTelefone] = useState();
-    // const [showAlert, setShowAlert] = useState(false);
-
+    let armazenaData = [];
+    
+    // const route = useRoute();
+    let [flatListItems, setFlatListItems] = useState([]);
+    // const [refresh, setRefresh] = useState(route.params?.setRefresh ? route.params.setRefresh : false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
 
     const PegarClientes = async () => {
         try {
-
-
             const response = await api.get(`/contatos`)
                 .catch(function (error) {
                     if (error.response) {
@@ -36,12 +42,14 @@ export default function TodosContatos() {
             console.log((response));
             if (response != undefined) {
                 if (response.data.length >= 1) {
-                    setAlertMessage('Registro inserido com sucesso!')
-                    setNome('');
-                    setEmail('');
-                    setCelular();
-                    setTelefone();
-                    handleShowAlert();
+
+                    for(let i = 0; i < response.data.length; i++) {
+                        armazenaData.push(response.data[i])
+                    }
+
+                    setFlatListItems(armazenaData)
+                    console.log(armazenaData)
+                    armazenaData = [];
                 }
                 else {
                     setAlertMessage('Ocorreu um erro ao inserir o registro');
@@ -52,16 +60,133 @@ export default function TodosContatos() {
             console.log(error)
         }
     }
+    const excluirCliente = async (id) => {
+        try {
+            const response = await api.delete(`/clientes/${id}`).catch(function (error) {
+                if (error.response) {
+                    console.error(error.response.data);
+                    console.error(error.response.status);
+                    console.error(error.response.headers);
+                    return;
+                } else if (error.request) {
+                    if ((error.request._response).includes('Failed')) {
+                        console.error(error.request._response);
+                        return;
+                    }
+
+                } else {
+                    console.error('Error:', error.message);
+                    return;
+                }
+
+            });
+
+            if (response != undefined) {
+
+                if (response.data.length >= 1) {
+
+                    setRefresh(prevState => !prevState);
+                    setAlertMessage('Registro excluído com sucesso!');
+                }
+                else {
+                    setAlertMessage('Registro não localizado');
+                }
+                handleShowAlert();
+            }
+            // console.log(cliente);
+        } catch (error) {
+            // console.log(error)
+            return;
+        }
+
+        let listItemView = (item) => {
+            return (
+    
+                <View
+                    key={item.id}
+                    style={{height:300,display:'flex',justifyContent:'space-between',alignItems:'center',flexDirection:'column', padding: 15, borderRadius: 10, shadowColor: 'black', elevation: 8 }}>
+                    <Text style={{fontSize:14}}>ID</Text>
+                    <Text style={{fontSize:16, fontWeight:'bold'}}>{item.id}</Text>
+    
+                    <Text style={{fontSize:14}}>Nome</Text>
+                    <Text style={{fontSize:16, fontWeight:'bold'}}>{item.nome}</Text>
+    
+                    <Text style={{fontSize:14}}>Celular</Text>
+                    <Text style={{fontSize:16, fontWeight:'bold'}}>{item.tel_cel}</Text>
+
+                    <Text style={{fontSize:14}}>Telefone fixo</Text>
+                    <Text style={{fontSize:16, fontWeight:'bold'}}>{item.tel_fixo}</Text>
+
+                    <Text style={{fontSize:14}}>E-mail</Text>
+                    <Text style={{fontSize:16, fontWeight:'bold'}}>{item.email}</Text>
+    
+                    <View style={{display:'flex',flexDirection:'row'}}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                Alert.alert(
+                                    'Atenção!',
+                                    'Deseja realmente excluir esse cliente?',
+                                    [
+                                        {
+                                            text: 'OK',
+                                            onPress: () => {
+                                                excluirCliente(item.id);
+                                                // setShowAlert(false)
+                                            }
+                                        },
+                                        {
+                                            text: 'Cancelar',
+                                            onPress: () => {
+                                                return;
+                                            }
+    
+                                        }
+                                    ],
+                                    //Permite clicar fora da áre do alert para fechá-lo;
+                                    { cancelable: true }
+                                )
+                            }}
+                            style={[styles.alignVH, { paddingRight: 30 }]}
+                        >
+    
+                            <FontAwesome5 name="trash-alt" color='red' size={24} />
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                navegaEditarCliente(item.id, item.nome, item.idade);    
+                            }}>
+                            <FontAwesome5 name="edit" color='blue' size={24} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+    
+            );
+        };
+
+
+    useFocusEffect(
+        useCallback(() => {
+            PegarClientes();
+        }, [])
+    )
 
     return (
         <SafeAreaView style={styles.container}>
            
-              
+           <View style={{ flex: 1, marginBottom: 10, width:'100%' }}>
+                <FlatList
+                    style={{ marginTop: 1 }}
+                    contentContainerStyle={{ paddingHorizontal: 20 }}
+                    data={flatListItems}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => listItemView(item)}
+                />
+            </View>
             
             <StatusBar style="auto" />
         </SafeAreaView>
     );
-}
+}}
 
 
 const styles = StyleSheet.create({
