7349512d827cd1a2d03a8da54b00ecf1d850cf6e
joao <joaowasconcelos18@gmail.com>
2024-03-19 21:47:57 -0300

    adicionando creditos

--- listaContatos/src/pages/Creditos/index.js
+++ listaContatos/src/pages/Creditos/index.js
@@ -1,38 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, ScrollView, StyleSheet, Text, View,Image } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function Creditos() {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View>
-          <View><Text>Murilo Aguiar</Text></View>
-          <View><Image
-          
-          ></Image></View>
-          <View>O Murilo foi responsavel de criar </View>
-        </View>
 
-        <View>
-          <View><Text>Lucas Manrick</Text></View>
-          <View><Image
-          style={styles.image} 
-          source={require("../../../assets/image.jpg")}
-          ></Image></View>
-          <View></View>
-        </View>
-
-        <View>
-          <View><Text>Murilo Aguiar</Text></View>
-          <View></View>
-          <View>O Murilo foi responsavel de criar </View>
-        </View>
+    <View style={styles.container}>
+      <View style={{ width: "90%", height: "20%", display: 'flex', flexDirection: 'row', backgroundColor: "#fdfdf9", alignItems: 'center' }}>
 
 
-        <StatusBar style="auto" />
+        <View style={styles.containerImage}><Image
+          style={styles.image}
+          source={require("../../assets/image.jpg")}
+        ></Image></View>
+
+        <View style={{ position: 'absolute', top: 0, left: 200 }}><Text>João Wasconcelos</Text></View>
+        <View><Text>O Murilo foi responsavel de criar </Text></View>
       </View>
-    </ScrollView>
+
+
+
+      <View style={styles.container}>
+        <View style={{ width: "90%", height: "30%", display: 'flex', flexDirection: 'row', backgroundColor: "#fdfdf9", alignItems: 'center' }}>
+
+
+          <View style={styles.containerImage}><Image
+            style={styles.image}
+            source={require("../../assets/murilo.jpg")}
+          ></Image></View>
+
+          <View style={{ position: 'absolute', top: 0, left: 200 }}><Text>Murilo Aguiar</Text></View>
+          <View style={{ width: "60%", height: "40%", display: 'flex', flexDirection: 'row', backgroundColor: "#fdfdf9", alignItems: 'center' }}><Text>O Murilo foi responsavel de criar a tela de cadastro e buscar contato </Text></View>
+        </View>
+
+      </View>
+
+
+
+
+
+
+
+
+
+
+
+
+      <StatusBar style="auto" />
+    </View>
+
   );
 }
 
@@ -41,7 +57,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    gap:70,
+    width:"100%"
+   
+
   },
-  
+
+  containerImage: {
+    width: "40%",
+    height: "100%",
+ 
+
+
+  },
+
+  image: {
+    width: "90%",
+    height: "100%",
+    backgroundColor: "blue",
+    borderRadius: 40,
+  }
+
 });
